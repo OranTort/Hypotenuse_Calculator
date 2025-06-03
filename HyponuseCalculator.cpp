0ca1544bc562cb2530df96917618adcf1740c209
Rich <HONGRICHARD89@GMAIL.COM>
2025-06-02 22:37:03 -0700

    create Hypotenuse Calculator

---
+++ HyponuseCalculator.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int main() {
+
+    double a;
+    double b;
+    double c;
+
+    std::cout << "Enter Side A: ";
+    std::cin >> a;
+
+    std::cout << "Enter Side B: ";
+    std::cin >> b;
+
+    //a = pow(a, 2);
+    //b = pow(b, 2);
+    //c = sqrt(a + b);
+
+    c = sqrt(pow(a, 2) + pow(b, 2));
+
+    std::cout << "Side C: " << c << "\n"; 
+
+    return 0;
+}
